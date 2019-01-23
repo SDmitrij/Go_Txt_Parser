f0e5598c4687458e32094680bf03875fafc35546
Dmitry <sidelevdima@yandex.ua>
2019-01-23 10:12:27 +0300

    add some format. output

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	files := initFileObjects(paths)
 
 	for _, file := range files {
-		fmt.Printf("File key: %s, file hash: %s\n", file.fileUniqueKey, file.fileHash)
+		fmt.Printf("File key: %s, file hash: %s, file path: %s, file size: %d\n",
+			file.fileUniqueKey, file.fileHash, file.filePath, file.fileSize)
 	}
 
 }
